cebaf75540dc18b98e6e19149250f380c4767e55
Mare12345678 <sonkenert@gmail.com>
2024-06-11 10:14:57 +0300

    Add files via upload
    Mare

---
+++ Drop the down.ps1
@@ -0,0 +1,78 @@
+﻿Add-Type -AssemblyName System.windows.Forms
+
+$FormObject=[System.Windows.Forms.Form]
+$LabelObject=[System.Windows.Forms.Label]
+$ComboBoxObject=[System.Windows.Forms.ComboBox]
+
+$DefaultFont='Verdana,12'
+
+
+##Set up base form
+$AppForm=New-Object $FormObject
+$AppForm.ClientSize='500,300'
+$AppForm.Text='Mare Imakson - Service Inspector'
+$AppForm.BackColor='#ffffff'
+$AppForm.Font=$DefaultFont
+
+#Building the form
+$lblService=New-Object $LabelObject
+$lblService.Text='Services :'
+$lblService.AutoSize=$true
+$lblService.Location=New-Object System.Drawing.Point(20,20)
+
+$ddlService=New-Object $ComboBoxObject
+$ddlService.width='300'
+$ddlService.Location=New-Object System.Drawing.Point(125,20) 
+
+#Load the drop down list for services
+Get-Service | ForEach-Object {$ddlService.Items.Add($_.Name)}
+
+$ddlService.text='Pick a service lollpea'
+
+$lblForName=New-Object $LabelObject
+$lblForName.Text='Service Friendly Name :'
+$lblForName.AutoSize=$true
+$lblForName.Location=New-Object System.Drawing.Point(20,80)
+
+$lblName=New-Object $LabelObject
+$lblName.Text='test'
+$lblName.AutoSize=$true
+$lblName.Location=New-Object System.Drawing.Point(240,80)
+
+$lblForStatus=New-Object $LabelObject
+$lblForStatus.Text='Status :'
+$lblForStatus.AutoSize=$true
+$lblForStatus.Location=New-Object System.Drawing.Point(20,120)
+
+$lblStatus=New-Object $LabelObject
+$lblStatus.Text=''
+$lblStatus.AutoSize=$true
+$lblStatus.Location=New-Object System.Drawing.Point(240,120)
+
+$AppForm.Controls.AddRange(@($lblService, $ddlService,$lblForName,$lblName,$lblForStatus,$lblStatus))
+
+##Add some functions to the form
+
+function GetServiceDetails{
+    $ServiceName=$ddlService.selectedItem
+    $details=Get-Service -Name $ServiceName | select *
+    $lblName.Text=$details.name
+    $lblStatus.Text=$details.status
+
+    if($lblStatus.text -eq 'Running'){
+        $lblStatus.ForeColor='green'
+
+    }else{
+       $lblStatus.ForeColor='red'
+
+    }
+}          
+##Add functions to the controls
+
+$ddlService.Add_SelectedIndexChanged({GetServiceDetails})
+
+#Show the form
+$AppForm.ShowDialog()
+
+##Garbage Collection
+$AppForm.Dispose()
---
+++ Test2.ps1
@@ -0,0 +1,45 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$FormObject = [System.Windows.Forms.Form]
+$LabelObject = [System.Windows.Forms.Label]
+$ButtonObject = [System.Windows.Forms.Button]
+
+$HelloWorldForm=New-Object $FormObject
+$HelloWorldForm.ClientSize='500,300'
+$HelloWorldForm.Text='Hello World - Tutorial'
+$HelloWorldForm.BackColor="#ffffff"
+
+$lbltitle=New-Object $LabelObject
+$lbltitle.Text='Hello world! '
+$lbltitle.AutoSize=$true
+$lbltitle.Font='Verdana, 24, style=Bold'
+$lbltitle.ForeColor='red'
+$lbltitle.Location=New-Object System.Drawing.Point(120,110)
+
+$btnHello=New-Object $ButtonObject
+$btnHello.Text='Say hello!'
+$btnHello.AutoSize=$true
+$btnHello.Font='Verdana,14'
+$btnHello.Location=New-Object System.Drawing.Point(175,180)
+
+$HelloWorldForm.Controls.AddRange(@($lbltitle,$btnHello))
+
+##Logic Section/Functions
+
+function SayHello{
+    if($lbltitle.Text -eq ''){
+        $lbltitle.Text='Hello World!'
+    }else{
+        $lbltitle.Text=''
+    }
+}
+
+
+##Add the functions to the form
+$btnHello.Add_Click({SayHello})
+
+#Display the form
+$HelloWorldForm.ShowDialog()
+
+##Cleans up the form
+$HelloWorldForm.Dispose()
